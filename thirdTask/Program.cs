77dbe7debbcf6f4a9f2743f8361e2659e9ed74f0
Mark <nedzelsky.mark@mail.ru>
2023-01-28 20:21:38 +0300

    third task solved

---
+++ thirdTask/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int GetNumber()
+{
+    int number;
+    
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            break;
+        }
+        else 
+        {
+            Console.WriteLine("Вы должны ввести число!");
+        }
+    }
+
+    return number;
+
+}
+
+void Show3DegreeNumbers(int highLimit)
+{
+    for (int i = 1; i <= highLimit; i++)
+    {
+        if (i == highLimit)
+        {
+            Console.Write($"{Math.Pow(i, 3)} ");
+        }
+        else
+        {
+            Console.Write($"{Math.Pow(i, 3)}, ");
+        }
+        
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int highLimit = GetNumber();
+Show3DegreeNumbers(highLimit);
